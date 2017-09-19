38882c9f50d79d65ca803d9e3af6ea9917af6b0e
Sebastian Poehn <poehn@online.de>
2017-09-19 09:47:43 +0200

    Be quiet if kubectl doesn't exist

--- completion.sh
+++ completion.sh
@@ -4,4 +4,4 @@
 [[ -s /usr/bin/aws_completer ]] && complete -C '/usr/bin/aws_completer' aws
 [ -f /usr/share/git-core/contrib/completion/git-prompt.sh ] && . /usr/share/git-core/contrib/completion/git-prompt.sh
 [ -r /etc/bash_completion.d/git ] && . /etc/bash_completion.d/git
-which kubectl > /dev/null && source <(kubectl completion bash)
+which kubectl &> /dev/null && source <(kubectl completion bash)
