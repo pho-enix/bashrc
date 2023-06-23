10921a15ec3da32b4ba41ac7884f67aae76bfdc4
Sebastian Poehn <19666623+pho-enix@users.noreply.github.com>
2023-06-23 10:54:41 +0200

    Add update alias

--- alias.sh
+++ alias.sh
@@ -8,5 +8,7 @@ git_link(){
 	git branch --set-upstream-to=origin/$1 $1
 }
 
-alias pwgen='pwgen -Bsync $[RANDOM%10+15] 1'
+alias pwgen='pwgen -Bsync $[RANDOM%15+20] 1'
+
+alias update='sudo apt update && sudo apt upgrade -y'
 
